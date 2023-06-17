02991b92c9472577ddf6f3a17528f27503ed1d90
Darrian Bagley <dbagle14@gmail.com>
2023-06-17 17:48:15 -0400

    fix: add base to src properties

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   outDir: "./docs",
-  site: "https://dbagley1.github.io/astro-darrian-dev/",
+  site: "https://dbagley1.github.io",
   base: '/astro-darrian-dev'
 });
