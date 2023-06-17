14e7f5c1fecfa0f2203a49d6c09c59f13e023cf9
Darrian Bagley <dbagle14@gmail.com>
2023-06-17 17:43:51 -0400

    fix: update url base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   outDir: "./docs",
-  site: "https://dbagley1.github.io/astro-darrian-dev/"
+  site: "https://dbagley1.github.io/astro-darrian-dev/",
+  base: '/astro-darrian-dev'
 });
