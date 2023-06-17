28e733bcefa99ae0eae33a1f21c8f79b4f768251
Darrian Bagley <dbagle14@gmail.com>
2023-06-17 18:05:32 -0400

    build: revert outDir to "dist"

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: "./docs",
   site: "https://dbagley1.github.io",
   base: '/astro-darrian-dev'
 });
