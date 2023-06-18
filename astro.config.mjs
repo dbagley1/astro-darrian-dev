f54660e0dbea24693f94804019d49569606b4909
Darrian Bagley <dbagle14@gmail.com>
2023-06-17 22:51:42 -0400

    feat: add tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://dbagley1.github.io",
-  base: '/astro-darrian-dev'
+  base: '/astro-darrian-dev',
+  integrations: [tailwind()]
 });
