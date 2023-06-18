8e11a9144f3b82b83083aaf325f3d625fc5d50e4
Darrian Bagley <dbagle14@gmail.com>
2023-06-18 05:11:42 -0400

    fix: remove relative period, set in article props

--- src/components/tailwind-ui/BlogSections.vue
+++ src/components/tailwind-ui/BlogSections.vue
@@ -2,7 +2,7 @@
   <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
     <article v-for="{ data: post, slug } in posts" :key="post.id" class="flex flex-col items-start justify-between">
       <div class="relative w-full">
-        <img :src="`.${post.img}`" alt="" class="object-top aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]" />
+        <img :src="`${post.img}`" alt="" class="object-top aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]" />
         <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
       </div>
       <div class="max-w-xl">
