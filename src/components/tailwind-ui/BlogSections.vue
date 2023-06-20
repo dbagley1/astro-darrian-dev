f89163d12423bf2370c57debf906a7df2a8561e9
Darrian Bagley <dbagle14@gmail.com>
2023-06-20 19:51:41 -0400

    feat: add link wrapper to project thumbnails

--- src/components/tailwind-ui/BlogSections.vue
+++ src/components/tailwind-ui/BlogSections.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-    <article v-for="{ data: post, slug } in posts" :key="post.id" class="flex flex-col items-start justify-between">
+    <article v-for="{ data: post, slug } in posts" :key="post.id" class="flex flex-col items-start">
       <div class="relative w-full">
-        <img :src="`${post.img}`" alt="" class="object-top aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]" />
-        <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+        <a :href="`./work/${slug}`">
+          <img :src="`${post.img}`" alt="" class="object-top aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]" />
+          <div class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+        </a>
       </div>
       <div class="max-w-xl">
         <!-- <div class="mt-8 flex items-center gap-x-4 text-xs">
